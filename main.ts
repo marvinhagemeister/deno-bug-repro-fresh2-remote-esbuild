c95c1f6d6bc2bbcf06d1f6db5dc5260903935f4b
Marvin Hagemeister <marvin@deno.com>
2024-06-26 12:15:58 +0200

    repro

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { App, fsRoutes, staticFiles } from "fresh";
+import { define, type State } from "./utils.ts";
+import { RemoteIsland } from "@marvinh-test/fresh-island";
+
+export const app = new App<State>();
+app.use(staticFiles());
+app.island("jsr:@marvinh-test/fresh-island", "RemoteIsland", RemoteIsland);
+
+// this is the same as the /api/:name route defined via a file. feel free to delete this!
+app.get("/api2/:name", (ctx) => {
+  const name = ctx.params.name;
+  return new Response(
+    `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
+  );
+});
+
+// this can also be defined via a file. feel free to delete this!
+const exampleLoggerMiddleware = define.middleware((ctx) => {
+  // console.log(`${ctx.req.method} ${ctx.req.url}`);
+  return ctx.next();
+});
+app.use(exampleLoggerMiddleware);
+
+await fsRoutes(app, {
+  dir: "./",
+  loadIsland: (path) => import(`./islands/${path}`),
+  loadRoute: (path) => import(`./routes/${path}`),
+});
+
+if (import.meta.main) {
+  await app.listen();
+}
